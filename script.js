ff0a8b953e45cc649821f22c7b09c22124a39ca7
타래 <bae94157617@gmail.com>
2021-07-05 21:11:03 +0900

    2110

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// 과제를 직접 해보자.
+const text = document.querySelector(".box div");
+
+const changeWord = {
+    ifClick: function () {
+        text.innerText = "I 'm Clicked !";        
+    },
+    ifMouseenter: function () {
+        text.innerText = "Mouse Hoverd !";        
+    },
+    ifMouseleave: function () {
+        text.innerText = "Mouse Leave ~";        
+    },
+    ifResize: function () {
+        text.innerText = "Window resized !";        
+    },
+    ifCopy: function () {
+        text.innerText = "Am I copied ?";        
+    }
+    
+};
+
+text.addEventListener("click", changeWord.ifClick);
+text.addEventListener("mouseenter", changeWord.ifMouseenter);
+text.addEventListener("mouseleave", changeWord.ifMouseleave);
+text.addEventListener("copy", changeWord.ifCopy);
+
+window.addEventListener("resize", changeWord.ifResize);
+
+console.dir(text);
