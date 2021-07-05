c95b11ad5f2e17529d2d58d2042d3c7a7c29a603
타래 <bae94157617@gmail.com>
2021-07-05 22:50:09 +0900

    change-color-red-blue

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-const text = document.querySelector(".box div");
+const text = document.querySelector(".box h1");
 
-const changeWord = {
-    ifClick: function () {
-        text.innerText = "I 'm Clicked !";        
-    },
-    ifMouseenter: function () {
-        text.innerText = "Mouse Hoverd !";        
-    },
-    ifMouseleave: function () {
-        text.innerText = "Mouse Leave ~";        
-    },
-    ifResize: function () {
-        text.innerText = "Window resized !";        
-    },
-    ifCopy: function () {
-        text.innerText = "Am I copied ?";        
-    }
-    
-};
+function ifmouseenter() {
+    text.classList.toggle("textBorderRed");
+}
+function ifmouseleave() {
+    text.classList.toggle("textBorderRed");
+}
 
-text.addEventListener("click", changeWord.ifClick);
-text.addEventListener("mouseenter", changeWord.ifMouseenter);
-text.addEventListener("mouseleave", changeWord.ifMouseleave);
-text.addEventListener("copy", changeWord.ifCopy);
+text.addEventListener("mouseenter", ifmouseenter);
+text.addEventListener("mouseleave", ifmouseleave);
 
-window.addEventListener("resize", changeWord.ifResize);
-
-console.dir(text);
