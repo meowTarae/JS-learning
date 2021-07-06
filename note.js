bc429bf802ca3a774efc64766227b5e5fa31cd5e
타래 <85001878+meowTarae@users.noreply.github.com>
2021-07-06 12:01:04 +0900

    Update note.js

--- note.js
+++ note.js
@@ -44,66 +44,64 @@
          console.log(7day[0~6]);  - 여기도 0부터 시작함.
 
 
-            Object에 에러있다 !
+            
 
         ~Object !  
         {/* player라는 Object의 property !     */}
         const player = {
-          name : "tarae",
-          cute : true,
+          name : "tarae",          
           age : 8,
-        };
+        };    <- 끝에 ; 붙음
 
          console.log(player.age);
          player.lastName = "Bae";  - 추가
         
 
+
+
+
+
+
         ~함수 !
-        {/* function name(params)               */}
-        function sayHello(nameOfPerseon, age) {
-          console.log("Hello i'm" + nameOfPerseon + "and" + age);          
-        }
+          function myFunc(a, b) {
+            console.log("Hi"+a+b);
+          }    <- 끝에 ; 안붙음
+          
+          myFunc()   ->  ()은 함수 실행 버튼으로 생각 !
+          myFunc(1,2);   -> ' Hi3 '
+              
 
-        sayHello("nico", 12);        
-          (print) -> Hello i'm nico and 12 age
 
 
-        ~응용 !
-          const calculate ={
-            name : "계산기", 
-            plus: function (firstNumber, secondNumber) {
-              console.log(firstNumber + secondNumber);
-            }
-            minus: function (firstNumber, secondNumber) {
-              console.log(firstNumber - secondNumber);
-            }
-            divide: function (firstNumber, secondNumber) {
-              console.log(firstNumber / secondNumber);
-            }
-            multiple: function (firstNumber, secondNumber) {
-              console.log(firstNumber * secondNumber);
-            }
-            powerof: function (firstNumber, secondNumber) {
-              console.log(firstNumber ** secondNumber);
-            }            
-          } 
 
-          calculate.plus(3, 5); 
-          calculate.minus(3, 5); 
-          calculate.divide(3, 5); 
-          calculate.multiple(3, 5); 
-          calculate.powerof(3, 5); 
+
+
+        ~Object 안에 함수를 넣어보자.            
+            const Calculate = {
+              plus: function (n1, n2) {
+                return n1 + n2;
+              },
+              minus: function (n1, n2) {
+                return n1 - n2;
+              },                              
+            };
+                
+            console.log(Calculate.plus(3, 5));
+        
 
 
 
 
           ~return !
           log로 출력시 값은 저장이 안되고 출력만 된다.
-          값을 저장하고싶다면, return을 쓰자.
+          값을 저장하고 싶다면, return을 쓰자.
 
            ex) powerof: function (firstNumber, secondNumber) {
                 return console.log(firstNumber ** secondNumber);
               }  
+
+
+
 
 
           ! syntax 
@@ -111,22 +109,46 @@
            isnan(~)  -> number면 flase, 아니면 true
  
           
+                 
+                 
+                 
 
           ~console에 document치면 html 코드가 나온당
-
+              ex) const a = document.querySelector(".list div");   
+                                                        -> 하나만 호출
+                  
+                  const a = document.querySelectorAll(".list div");
+                                                        ->   전체 호출
+                                                          
+                                                        
+                                                            
           
-          if (age === 100) {
-            ~~
-          } else if (true) {
-            ~~
+          ~dir
+          console.dir(a);   ->  요소 a의 내부를 볼 수 있음
+          
+          
+                    
+          
+          
+          ~Event
+          const text = document.querySelector(".box h2");
+          
+          function ifClick() {
+            text.classList.toggle("ifClicked");
+                                  -> ifClicked는 css에서 내가 적은 style중 하나임.
+            text.style.color = "royalblue";                      
           }
-  
+
+          text.addEventListener("click", ifClick);
+                                  -> click 시 ifClick함수가 실행되는거라 ()안붙인다.
 
 
+                                  
  
 
-
-
+         h1.classname = "A";               -> 
+         h1.classname = "";                -> 
+         h1.getElementByClassName = "A";   -> 
 
 
 
